7de7cdbe83a0db0bfdb5887d40d08eef9044286d
Your Name <you@example.com>
2023-12-14 18:45:58 -0500

    fixed repo using professor recommendations

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,5 +1,7 @@
 /* eslint-disable func-names */
-const fetch = require('node-fetch');
+// mod.cjs
+// eslint-disable-next-line no-shadow
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 exports.handler = async function () {
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
